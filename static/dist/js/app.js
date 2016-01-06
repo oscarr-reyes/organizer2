a2557b8f6fa9e7b19367c9c8750a072252981325
Nosthertus <darkfreak17@hotmail.com>
2016-01-06 05:00:44 -0430

    New: Main view and MainController for angular, fixed: Depedendecies of ngMaterial
    *Added main view for layout
    *Added MainController for the main control of the app
    *Loaded dependecies of ngMaterial to fix it's bootstrap

--- static/dist/js/app.js
+++ static/dist/js/app.js
@@ -1,3 +1,18 @@
 (function(angular){
-	var app = angular.module('organizer', []);
+	var app = angular.module('organizer', ['ngMaterial', 'ui.router']);
+
+	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+		// redirect to / when there is no state that matches with the provided url
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider.state('main', {
+			url: '/',
+			views: {
+				'@':{
+					templateUrl: 'views/layout/main.html',
+					controller: 'MainController' 
+				}
+			}
+		});
+	}]);
 })(angular);
---
+++ static/dist/js/controllers/MainController.js
@@ -0,0 +1,7 @@
+(function(angular){
+	var app = angular.module('organizer');
+
+	app.controller('MainController', ['$scope', '$state', function($scope, $state){
+		
+	}]);
+})(angular);
